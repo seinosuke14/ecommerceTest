c3b2ebcc5c22ae3dae66cae78d534c14b6accfbb
seinosuke14 <diegobascur9@gmail.com>
2025-12-02 18:50:50 -0300

    creacion de menu

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,8 @@ import Products from './products';
 import Categoria from './categoria';
 import Productoxcategoria from './productoxcategoria';
 import ProductImg from './productimg';
-
+import Menu from './menu';
+import MenuProducto from './menuxproducto';
 
 
 
@@ -21,8 +22,34 @@ Categoria.belongsToMany(Products, {
     as: 'productos'
 });
 
+
+Menu.belongsToMany(Products, {
+    through: MenuProducto,
+    foreignKey: 'menu_id',
+    otherKey: 'producto_id',
+    as: 'productos'
+});
+
+Products.belongsToMany(Menu, {
+    through: MenuProducto,
+    foreignKey: 'producto_id',
+    otherKey: 'menu_id',
+    as: 'menus'
+});
+
+// Relaciones adicionales para acceder a cantidad y orden
+Menu.hasMany(MenuProducto, { foreignKey: 'menu_id', as: 'menu_productos' });
+Products.hasMany(MenuProducto, { foreignKey: 'producto_id' });
+
+MenuProducto.belongsTo(Menu, { foreignKey: 'menu_id' });
+MenuProducto.belongsTo(Products, { foreignKey: 'producto_id', as: 'producto' });
+
 Products.hasMany(ProductImg, { foreignKey: 'product_id', as: 'imagenes' });
 ProductImg.belongsTo(Products, { foreignKey: 'product_id', as: 'producto' });
+
+
+
+
 
 
 
